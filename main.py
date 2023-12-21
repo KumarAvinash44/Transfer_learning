0f8de8c237c4d6f05f42096e1b3b2cef0f93a198
Avinash Kumar <kumar@soongsil.ac.kr>
2023-12-21 15:54:33 +0900

    pytorch model

--- main.py
+++ main.py
@@ -15,3 +15,52 @@ from tempfile import TemporaryDirectory
 cudnn.benchmark = True
 plt.ion()
 
+
+# Data augmentation and normalization for training
+# just normalization for validation
+
+data_transforms = {
+    "train": transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    "val": transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+data_dir = './hymenoptera_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size = 4, shuffle = True, num_workers = 1) for x in ['train', 'val']}
+
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', "val"]}
+
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f'The used device is {device}')
+
+def imshow(inp, title=None):
+    """Display image for Tensor."""
+
+    inp = inp.numpy().transpose((1,2,0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = inp.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001) # pause a bit so that plot are updated
+
+# inputs, classes = next(iter(dataloaders['train']))
+
+# out = torchvision.utils.make_grid(input)
+
+# imshow(out, title=[class_names[x] for x in classes])
